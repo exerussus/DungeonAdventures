3f364c912d89dbab5aa43d5e0188b10843d4d503
exerussus <solovyov.production@gmail.com>
2022-04-08 15:10:29 +0300

    Исправлен баг в методе change класса Location.

--- classes.py
+++ classes.py
@@ -113,7 +113,8 @@ class Location:
 
     def change(self, decision):
         """Изменяет локацию"""
-        actually = data_location['actually_location']
+        actually = data_location['location']['actually_location']
         data_location['actually_location'] = data_location['location'][actually][decision]
+
 
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
---
+++ test.py
@@ -0,0 +1,4 @@
+from data import data_location
+
+actually = data_location['location']['actually_location']
+print(data_location['location'][actually])
