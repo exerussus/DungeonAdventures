ce90a463ddde28d63397814eba97acc86209a081
exerussus <solovyov.production@gmail.com>
2022-04-08 13:40:55 +0300

    Модифицирован скрипт

--- script.py
+++ script.py
@@ -1,11 +1,16 @@
 import pyautogui
 import time
 
-x = 33
+x = 3
 time.sleep(5)
+pyautogui.write(str(x))
+with pyautogui.hold('ctrl'):
+    pyautogui.press('left', presses=1, interval=0.01)
+x += 1
 while x != 600:
     pyautogui.press('down', presses=6, interval=0.01)
-    pyautogui.press('left', presses=1, interval=0.01)
-    pyautogui.press('backspace', presses=1, interval=0.01)
     pyautogui.write(str(x))
+    with pyautogui.hold('ctrl'):
+        pyautogui.press('left', presses=1, interval=0.01)
+
     x += 1
