30ea36f879d1c6b140697b6a804c275570321cdd
exerussus <solovyov.production@gmail.com>
2022-04-08 13:32:09 +0300

    Увеличена скорость работы скрипта.

--- script.py
+++ script.py
@@ -4,8 +4,8 @@ import time
 x = 33
 time.sleep(5)
 while x != 600:
-    pyautogui.press('down', presses=6, interval=0.05)
-    pyautogui.press('left', presses=1, interval=0.05)
-    pyautogui.press('backspace', presses=1, interval=0.05)
+    pyautogui.press('down', presses=6, interval=0.01)
+    pyautogui.press('left', presses=1, interval=0.01)
+    pyautogui.press('backspace', presses=1, interval=0.01)
     pyautogui.write(str(x))
     x += 1
